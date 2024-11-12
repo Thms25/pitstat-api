c21b86b7dc190f797466153ae8dc619ee973a48f
Thomas Allen <thomasallenmartinho@gmail.com>
2024-11-12 18:08:43 +0100

    added requirements txt

--- app/api/endpoints/drivers.py
+++ app/api/endpoints/drivers.py
@@ -1,16 +1,34 @@
 from fastapi import APIRouter, HTTPException
-# import fastf1
+import fastf1
+import datetime
 
 router = APIRouter()
 
 @router.get("/drivers")
 async def read_drivers():
     print("Reading drivers")
+    today = datetime.date.today()
+    year = today.year
+
+    calendar = fastf1.get_event_schedule(year, include_testing=False, backend=None, force_ergast=False).to_dict()
+
+    for event in calendar['EventName']:
+        race_event = fastf1.get_session(year, event, 'R', backend=None, force_ergast=False)
+        print('---')
+        # session = race_event.get_race()
+        # print(session)
 
     return [
-        {"name": "John Doe", "id": 1},
-        {"name": "Alice", "id": 2},
+        {"name": "Lewis Hamilton", "id": 44},
+        {"name": "Valtteri Bottas", "id": 77},
+        {"name": "Max Verstappen", "id": 33},
+        {"name": "Sergio Perez", "id": 11},
+        {"name": "Daniel Ricciardo", "id": 3},
+        {"name": "Lando Norris", "id": 4},
+        {"name": "Charles Leclerc", "id": 16},
+        {"name": "Carlos Sainz", "id": 55},
     ]
+
     
 @router.get("/drivers/{driver_id}")
 def read_driver(driver_id: int):
--- app/api/endpoints/races.py
+++ app/api/endpoints/races.py
@@ -3,15 +3,26 @@ import fastf1
 
 router = APIRouter()
 
+async def get_schedule(year: int):
+    return fastf1.get_event_schedule(year, include_testing=False, backend=None, force_ergast=False)
+
 @router.get("/races")
 async def read_races(year: int = 2024):
-    
-    print(f"Fetching calendar for {year}")
-    calendar = fastf1.get_event_schedule(year, include_testing=False, backend=None, force_ergast=False)
+    calendar = await get_schedule(year)
     return calendar.to_dict(orient='records')
 
 @router.get("/races/{round}")
 async def read_race(round: int, year: int = 2024):
-    races = fastf1.get_event_schedule(year, include_testing=False, backend=None, force_ergast=False)
+    races = await get_schedule(year)
     race = races.get_event_by_round(round)
-    return race.to_dict()
+    race_session = race.get_race()
+    race_session.load()
+    results = race_session.results
+    laps = race_session.laps
+    fastest_lap = laps.pick_fastest()
+
+    return {
+        "race_info" :race.to_dict(),
+        "results": results.to_dict(orient='records'),
+        "fastest_lap": fastest_lap.to_dict()
+        }
